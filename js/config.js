8c1eea4ba4856a065e0703d40a94dee57085ae11
wawawa1023 <miyaryousuke1023@gmail.com>
2025-09-13 17:39:03 +0900

    configオブジェクトにプロパティ追加

--- js/config.js
+++ js/config.js
@@ -7,11 +7,19 @@ const GAME_STATES = {
 };
 
 const CONFIG = {
+    // キャンバス設定
+    CANVAS_WIDTH: 600,
+    CANVAS_HEIGHT: 400,
+    CELL_SIZE: 20,
+    
+    // スコア設定
     SCORES: {
         DOT: 10,
         POWER_PELLET: 50,
         GHOST: 200,
     },
+    
+    // 色設定
     COLORS: {
         PLAYER: "yellow",
         GHOSTS: {
