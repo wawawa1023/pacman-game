a9edae4997887f8a930f2140de19700be9f44ab2
wawawa1023 <miyaryousuke1023@gmail.com>
2025-09-13 17:26:52 +0900

    add config.js

---
+++ js/config.js
@@ -0,0 +1,24 @@
+// js/config.js
+const GAME_STATES = {
+    MENU: "menu",
+    PLAYING: "playing",
+    PAUSED: "paused",
+    GAME_OVER: "game_over",
+};
+
+const CONFIG = {
+    SCORES: {
+        DOT: 10,
+        POWER_PELLET: 50,
+        GHOST: 200,
+    },
+    COLORS: {
+        PLAYER: "yellow",
+        GHOSTS: {
+            RED: "red",
+            PINK: "pink",
+            CYAN: "cyan",
+            ORANGE: "orange",
+        },
+    },
+};
